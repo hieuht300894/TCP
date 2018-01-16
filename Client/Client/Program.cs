a138cd1626c859d2dbab10451dd65ed88a10cdb9
hieuht300894 <30399541+hieuht300894@users.noreply.github.com>
2018-01-16 13:30:47 +0700

    a

---
+++ Client/Client/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        public static int BufferSize = 1024;
+        public static int PortNumber = 9999;
+        public static IPAddress AddressIP = IPAddress.Parse("127.0.0.1");
+
+        public static void Main()
+        {
+            try
+            {
+                TcpClient client = new TcpClient();
+
+                // 1. connect
+                client.Connect(AddressIP, PortNumber);
+                Stream stream = client.GetStream();
+                StreamReader reader = new StreamReader(stream);
+                StreamWriter writer = new StreamWriter(stream) { AutoFlush = true };
+
+                Console.WriteLine($"Client started on {((IPEndPoint)client.Client.LocalEndPoint).ToString()}");
+                Console.WriteLine($"Connected to {((IPEndPoint)client.Client.RemoteEndPoint).ToString()}");
+
+                while (true)
+                {
+                    // 2. send
+                    Console.Write("Enter your name: ");
+                    string strSend = Console.ReadLine().TrimEnd('\0');
+                    writer.WriteLine(strSend);
+
+                    if (strSend.ToLower().StartsWith("break"))
+                        break;
+
+                    // 3. receive
+                    string strRead = reader.ReadLine().TrimEnd('\0');
+                    Console.WriteLine($"Received: {strRead}");
+                }
+
+                // 4. Close    
+                Console.WriteLine($"Disconnected to {((IPEndPoint)client.Client.RemoteEndPoint).ToString()}");
+                stream.Close();
+                client.Close();
+                Console.Read();
+            }
+
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Error: " + ex);
+                Main();
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        public static int BufferSize = 1024;
+        public static int PortNumber = 9999;
+        public static IPAddress AddressIP = IPAddress.Parse("127.0.0.1");
+
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+        async static void Start()
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                try
+                {
+                    TcpListener listener = new TcpListener(AddressIP, PortNumber);
+
+                    // 1. listen
+                    listener.Start();
+                    Console.WriteLine($"Server started on {((IPEndPoint)listener.LocalEndpoint).ToString()}");
+                    Console.WriteLine("Waiting for a connection...");
+
+                    Socket socket = listener.AcceptSocket();
+                    Stream stream = new NetworkStream(socket);
+                    StreamReader reader = new StreamReader(stream);
+                    StreamWriter writer = new StreamWriter(stream) { AutoFlush = true };
+                    Console.WriteLine($"Connection received from {((IPEndPoint)socket.RemoteEndPoint).ToString()}");
+
+                    while (true)
+                    {
+                        // 2. receive
+                        string strReceive = reader.ReadLine().TrimEnd('\0');
+                        Console.WriteLine($"Received: {strReceive}");
+
+                        if (strReceive.ToLower().StartsWith("break"))
+                            break;
+
+                        // 3. send
+                        string strSend = $"Hello {strReceive}";
+                        writer.WriteLine(strSend);
+                        Console.WriteLine($"Sended: {strSend}");
+                    }
+
+                    // 4. close
+                    Console.WriteLine($"Disconnection received from {((IPEndPoint)socket.RemoteEndPoint).ToString()}");
+                    stream.Close();
+                    socket.Close();
+                    listener.Stop();
+                }
+                catch (Exception ex)
+                {
+                    Start();
+                }
+
+                Console.Read();
+            });
+        }
+    }
+}
